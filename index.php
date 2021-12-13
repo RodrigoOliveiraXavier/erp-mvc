17205bdeb3be238c08d7954bc5aeefb523b9e276
RodrigoOliveiraXavier <rodrigo.oliveira.xavier151@gmail.com>
2021-12-13 09:47:10 -0300

    Adicionado validação de ambiente para ativar o display errors do PHP

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-//Define as configurações de erros
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-
 //Valida se os arquivos de log do PHP estão criados
 if (!is_dir('tmp/')) {
   mkdir('tmp/');
@@ -14,14 +10,24 @@ if (!is_file('tmp/php_errors.log')) {
 //Seta o arquivo de log do php
 ini_set('error_log', 'tmp/php_errors.log');
 
-//Define as constantes
+//Define a constante de configuraçães da aplicação
 define('APPLICATION', parse_ini_file(__DIR__ . '/app/config/application.ini', true));
+//Define o ambiente da aplicação
 define('ENVIRONMENT', 'development');
+//Define o caminha da pasta raiz da aplicação (apenas para apoio no código)
 define('__PATH__', __DIR__);
+//Define a classe de autoload do PHP
 define('AUTOLOAD_CLASS', __DIR__ . '/vendor/autoload.php');
 
-//Inclui a classe de autoload e a classe para conexão do banco
+//Caso ambiente de desenvolviento define as configurações de erros
+if (ENVIRONMENT == 'development') {
+  error_reporting(E_ALL);
+  ini_set('display_errors', true);
+}
+
+//Inclui a classe de autoload
 require_once(AUTOLOAD_CLASS);
+//Inclui o arquivo de conexão do banco
 require_once(__DIR__ . '/app/config/config.php');
 
 //Inicia a sessão do usuario
